5a44b19e9a6ba6c6d4521139474a52fcc54633ed
ori petel <oripetel1306@gmail.com>
2016-12-24 16:11:27 +0200

    Finish project - new ps

--- ps.py
+++ ps.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 import os
-def filter_pid():
-    '''filter all subdirectories of /proc, in
-    order to find all the processes
+def filter_processes():
+    '''filter all subdirectories of /proc,
+    in order to find all the processes
     '''
     pids=[]
-    for pid in os.listdir('/proc'):
+    for process in os.listdir('/proc'):
         try:
-            int(pid)
-            pids.append(pid)
+            int(process)
+            pids.append(process)
         except ValueError:
             pass
     return pids
+def ps():
+    '''print this information for all running processes:
+        1. pid
+        2. ppid
+        3. comm
+        4. utime
+        5. num_threads
+        6. processor
+        7. vsize
+    '''
+    for pid in filter_processes():
+        stat = open('/proc/{}/stat'.format(pid)).read().split()
+        print ('pid: {},  ppid: {},  comm: {},  utime: {},  '
+        + 'num_threads: {},  processor: {},  vsize: {}').format(pid, stat[3], stat[1][1:-1], stat[13], stat[19], stat[38], stat[22])
+ps()
