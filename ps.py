b22873866e86e0ada960d81b25df0c49ca28f30a
ori petel <oripetel1306@gmail.com>
2016-12-24 14:54:56 +0200

    use filter

--- ps.py
+++ ps.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python
+import os
+def filter_pid():
+    '''filter all subdirectories of /proc, in
+    order to find all the processes
+    '''
+    pids=[]
+    for pid in os.listdir('/proc'):
+        try:
+            int(pid)
+            pids.append(pid)
+        except ValueError:
+            pass
+    return pids
